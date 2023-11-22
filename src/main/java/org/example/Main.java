4aa0f94ccecc746b5ebc4aaaf87ab2903f0d20bd
Basia <b4605634@gmail.com>
2023-11-22 09:50:31 +0100

    reformated -main (printLine)

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,12 +5,12 @@ import java.util.List;
 
 
 public class Main {
-    public static void main(String[] args) throws FileNotFoundException {
-        final String ANSI_RESET = "\u001B[0m";
-        final String ANSI_YELLOW = "\u001B[33m";
-        System.out.println("Hello world!+\n");
-        Simulation simulation = new Simulation();
 
+    private static final String ANSI_RESET= "\u001B[0m";
+    private static final String ANSI_YELLOW= "\u001B[33m";
+
+    public static void main(String[] args) throws FileNotFoundException {
+        Simulation simulation = new Simulation();
 
         List<Item> u1Phase1 = simulation.loadItems(1, "U1");
         List<Rocket> u1Rockets = simulation.load(u1Phase1, "U1");
@@ -20,7 +20,6 @@ public class Main {
         List<Rocket> u1RocketsP2 = simulation.load(u1Phase2,"U1");
         int[] u1Phase2Results = simulation.runSimulation(u1RocketsP2);
 
-
         List<Item> u2Phase1 = simulation.loadItems(1, "U2");
         List<Rocket> u2RocketsP1 = simulation.load(u2Phase1,"U2");
         int[] u2Phase1Results = simulation.runSimulation(u2RocketsP1);
@@ -29,24 +28,17 @@ public class Main {
         List<Rocket> u2RocketsP2 = simulation.load(u2Phase2,"U2");
         int[] u2Phase2Results = simulation.runSimulation(u2RocketsP2);
 
-        System.out.println("\n\n");
-        System.out.println(ANSI_YELLOW+"-".repeat(40) + "OVERVIEW" + "-".repeat(39)+ANSI_RESET);
-        System.out.printf("|Phase  | %37s |%37s |%n", "*********** Rocket U1 *************",  "********** Rocket U2 **********");
-        System.out.printf("|       |  %4s | %13s | %13s | %4s | %13s | %13s |%n", "Cost", "Tot launched", "incl crashed", "Cost", "Tot launched", "incl crashed");
-        System.out.println("-".repeat(87));
-        System.out.printf("|Phase 1|  %4d | %13d | %13d | %4d | %13d | %13d |%n", u1Phase1Results[0], u1Phase1Results[1], u1Phase1Results[2], u2Phase1Results[0], u2Phase1Results[1], u2Phase1Results[2]);
-        System.out.printf("|Phase 2|  %4d | %13d | %13d | %4d | %13d | %13d |%n", u1Phase2Results[0], u1Phase2Results[1], u1Phase2Results[2], u2Phase2Results[0], u2Phase2Results[1], u2Phase2Results[2]);
-        System.out.println("-".repeat(87));
-        System.out.printf("|Total  |  %4d | %13d | %13d | %4d | %13d | %13d |%n", u1Phase2Results[0]+u1Phase2Results[0], u1Phase1Results[1]+u1Phase2Results[1], u1Phase1Results[2]+u1Phase2Results[2],u2Phase1Results[0]+u2Phase2Results[0], u2Phase1Results[1]+ u2Phase2Results[1],u2Phase1Results[2]+ u2Phase2Results[2]);
-        System.out.println(ANSI_YELLOW+"-".repeat(87)+ANSI_RESET);
+        printPhasesOverview(u1Phase1Results, u1Phase2Results, u2Phase1Results, u2Phase2Results);
 
-        System.out.println(ANSI_YELLOW+"Total weight of equipment transported to the Moon:\n\n"+ANSI_RESET +
-                "Phase 1 "+"U1: "+u1Phase1Results[3]+" U2:"+u2Phase1Results[3]+"\n"+
-                "Phase 2 "+"U1: "+u1Phase2Results[3]+" U2:"+u2Phase2Results[3]+"\n");
-        System.out.println(ANSI_YELLOW+"-".repeat(87)+ANSI_RESET);
+        printTotalTransported( u1Phase1Results, u1Phase2Results, u2Phase1Results, u2Phase2Results);
 
-        System.out.println(ANSI_YELLOW + "Details of crashed rockets:" + ANSI_RESET);
-        System.out.println("_".repeat(87));
+        printDetailsOfCrashedRockets();
+    }
+
+    private static void printDetailsOfCrashedRockets() {
+
+        printLine(true, "Details of crashed rockets:");
+        printLine(true,"_".repeat(87) );
         String list = "|Rocket:  %5s |cost: %2s |cargo weight: %5s |tot weight: %5s |no of rockets: %2s |\n";
 
         int u1Garbage = 0;
@@ -60,15 +52,43 @@ public class Main {
                 } else if (r instanceof U2) {
                     u2Garbage += (r.getRocketWeightKg() + r.currentWeight);
                 } else {
-                    System.out.println(" what kind of rocket was crashed? ");
+                    printLine(false," what kind of rocket was crashed? ");
                 }
             }
         } else {
-            System.out.println("No rocket was crashed ");
+            printLine(false,"No rocket was crashed ");
         }
-        System.out.println("-".repeat(87));
+        printLine(true,"_".repeat(87) );
+        printLine(true, "Total garbage ");
+        printLine(false,"U1: "+u1Garbage+"\n"+"U2: "+u2Garbage);
+    }
 
-        System.out.println(ANSI_YELLOW + "Total garbage " + ANSI_RESET);
-        System.out.println("U1: "+u1Garbage+"\n"+"U2: "+u2Garbage);
+      private static void printLine(boolean colour, String text) {
+        if (colour) {
+            System.out.println(ANSI_YELLOW + text + ANSI_RESET);
+        } else {
+            System.out.println(text);
+        }
+    }
+
+
+    private static void printTotalTransported( int[] u1Phase1Results, int[] u1Phase2Results, int[] u2Phase1Results, int[] u2Phase2Results) {
+        System.out.println(ANSI_YELLOW +"Total weight of equipment transported to the Moon:\n\n"+ ANSI_RESET +
+                "Phase 1 "+"U1: "+ u1Phase1Results[3]+" U2:"+ u2Phase1Results[3]+"\n"+
+                "Phase 2 "+"U1: "+ u1Phase2Results[3]+" U2:"+ u2Phase2Results[3]+"\n");
+        printLine(true,"_".repeat(87) );
+    }
+
+    private static void printPhasesOverview(int[] u1Phase1Results, int[] u1Phase2Results, int[] u2Phase1Results, int[] u2Phase2Results) {
+        printLine(false,"\n\n");
+        printLine(true, "-".repeat(40) + "OVERVIEW" + "-".repeat(39));
+        System.out.printf("|Phase  | %37s |%37s |%n", "*********** Rocket U1 *************",  "********** Rocket U2 **********");
+        System.out.printf("|       |  %4s | %13s | %13s | %4s | %13s | %13s |%n", "Cost", "Tot launched", "incl crashed", "Cost", "Tot launched", "incl crashed");
+        printLine(false,"_".repeat(87) );
+        System.out.printf("|Phase 1|  %4d | %13d | %13d | %4d | %13d | %13d |%n", u1Phase1Results[0], u1Phase1Results[1], u1Phase1Results[2], u2Phase1Results[0], u2Phase1Results[1], u2Phase1Results[2]);
+        System.out.printf("|Phase 2|  %4d | %13d | %13d | %4d | %13d | %13d |%n", u1Phase2Results[0], u1Phase2Results[1], u1Phase2Results[2], u2Phase2Results[0], u2Phase2Results[1], u2Phase2Results[2]);
+        printLine(false,"_".repeat(87) );
+        System.out.printf("|Total  |  %4d | %13d | %13d | %4d | %13d | %13d |%n", u1Phase2Results[0]+ u1Phase2Results[0], u1Phase1Results[1]+ u1Phase2Results[1], u1Phase1Results[2]+ u1Phase2Results[2], u2Phase1Results[0]+ u2Phase2Results[0], u2Phase1Results[1]+ u2Phase2Results[1], u2Phase1Results[2]+ u2Phase2Results[2]);
+        printLine(true,"_".repeat(87) );
     }
 }
